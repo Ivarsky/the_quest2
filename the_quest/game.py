fefd1a4ce5ab9b936959b43db3d6b7e9ada74ee2
Ivarsky <iamh90@gmail.com>
2022-09-06 18:30:06 +0000

    feat: modo pantalla establecido

--- the_quest/game.py
+++ the_quest/game.py
@@ -1,6 +1,14 @@
+from turtle import width
+import pygame
+
+from the_quest import HEIGHT, WIDTH
+
+
 class TheQuest:
     def __init__(self):
         print("Starting game")
+        pygame.init()
+        pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 if __name__ == "__main__":
